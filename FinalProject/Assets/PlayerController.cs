f9a03851ac275f3cb90101ef935e802d49dd8d46
CelestialMagic <116318917+CelestialMagic@users.noreply.github.com>
2022-10-21 16:54:09 -0700

    Added PlayerController Script
    The player character can now jump

---
+++ FinalProject/Assets/PlayerController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip m_JumpSound;
+
+    [SerializeField]
+    private AudioSource m_AudioSource;
+
+    [SerializeField]
+    private float m_SpeedForce;
+
+    [SerializeField]
+    private float m_JumpForce;
+
+    [SerializeField]
+    private Rigidbody2D m_Rigidbody;
+
+    private Vector3 m_ToApplyMove;
+
+    private Animator m_Anim;
+    private SpriteRenderer m_Renderer;
+
+    private bool isGrounded; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (m_Anim != null)
+        //{
+        //    m_Anim.SetBool("Ground", true);
+        //    m_Anim.SetFloat("Speed", 0);
+        //}
+
+        m_Renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Potential Swim Code
+        //if (Input.GetKey(KeyCode.D)){
+        //    m_ToApplyMove += new Vector3(m_SpeedForce, 0, 0);
+        //}
+
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    m_ToApplyMove += new Vector3(-m_SpeedForce, 0, 0);
+        //}
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_ToApplyMove += new Vector3(0, m_JumpForce, 0);
+        }
+
+
+    }
+    void FixedUpdate()
+    {   //Alternate Jump
+        //transform.position += 
+        m_Rigidbody.AddForce(m_ToApplyMove);
+        m_ToApplyMove = Vector3.zero;
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
