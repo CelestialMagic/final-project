de02d16075942ee5407d1c1cbdc954250af01393
CelestialMagic <116318917+CelestialMagic@users.noreply.github.com>
2022-10-27 10:48:46 -0700

    Created TitleScreen Canvas
    -Organized assets into folders
    -Added title text and button to title screen

--- FinalProject/Assets/Scripts/GameStateManager.cs
+++ FinalProject/Assets/Scripts/GameStateManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameStateManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject currentMenu;
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +22,10 @@ public class GameStateManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
