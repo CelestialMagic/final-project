9d193df8954127fd5a987f394d1d04ad69556730
CelestialMagic <116318917+CelestialMagic@users.noreply.github.com>
2022-11-04 10:46:12 -0700

    Commented More

--- FinalProject/Assets/Scripts/GameStateManager.cs
+++ FinalProject/Assets/Scripts/GameStateManager.cs
@@ -13,19 +13,19 @@ public class GameStateManager : MonoBehaviour
     [SerializeField]
     private GameObject GameOverScreen; //A reference to the GameObject that is the GameOver UI Screen
     [SerializeField]
-    private float obstacleMoveSpeed; //This field is exposed in the editor but private to the class, this allows us to adjust the move speed of the pilars in the editor
+    private float obstacleMovespeed; //This field is exposed in the editor but private to the class, this allows us to adjust the move speed of the pilars in the editor
 
     [SerializeField]
-    private List<String> m_Levels = new List<string>();
+    private List<String> m_Levels = new List<string>(); // A list of levels in the form of strings
 
     [SerializeField]
-    private string m_TitleSceneName;
+    private string m_TitleSceneName;//A string representing the name of the Title Scene
 
-    private static GAMESTATE m_State;
+    private static GAMESTATE m_State; //The current game state
 
     private static GameStateManager _instance; //This class is a Singleton - We will also discuss this pattern later in this class.
 
-
+    //An enum that represents the game states. 
     enum GAMESTATE
     {
         MENU,
@@ -56,12 +56,14 @@ public class GameStateManager : MonoBehaviour
 
 
         //Put other inialization for you game state here
-        ObstacleMoveSpeed = obstacleMoveSpeed;
+        ObstacleMoveSpeed = obstacleMovespeed;
         m_State = GAMESTATE.MENU;
 
 
     }
 
+    //GetGameState() was created with the intention of retrieving the game state
+    //from the GameStateManager. It uses a switch statement. 
     public static string GetGameState(){
 
         return m_State switch
@@ -74,21 +76,18 @@ public class GameStateManager : MonoBehaviour
         };
     }
 
-
-    //Alpha Playtest GameOver() code
+    //LevelGameOver() is eventually going to be used to advance between levels.
+    //It is not a final game over, but rather, a pause to let players advance. 
     public static void LevelGameOver()
     {
-
-        //Add any logic that you would want to do when the game ends here
-        //This invokes the game over screen - here we are calling all the methods that subscribed to this action.
-        //OnGameOver();
-        Debug.Log(m_State);
         m_State = GAMESTATE.GAMEOVER;
-        Debug.Log(m_State);
-        ObstacleMoveSpeed = 2;
-        Menu();
+        ObstacleMoveSpeed = 0;
+        //Menu();
     }
 
+
+
+    //ResetScene() is responsible for resetting the current scene through a reload.
     public static void ResetScene()
     {
         
@@ -96,28 +95,23 @@ public class GameStateManager : MonoBehaviour
         m_State = GAMESTATE.PLAYING;
     }
 
-    public static void GameOver()
-    {
+  
 
-        //Add any logic that you would want to do when the game ends here
-        //This invokes the game over screen - here we are calling all the methods that subscribed to this action.
-        m_State = GAMESTATE.MENU; 
-        SceneManager.LoadScene(_instance.m_TitleSceneName);
-
-    }
-
-    //Used to advance to the next scene 
+    //Used to advance to the next scene
     public static void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
     }
 
+    //StartGame() is used to start the game.
+    
     public static void StartGame()
     {
         m_State = GAMESTATE.PLAYING;
         NextLevel();
     }
 
+    //TogglePause() will be used later to pause the game. 
     public static void TogglePause()
     {
         if(m_State == GAMESTATE.PLAYING)
@@ -131,10 +125,12 @@ public class GameStateManager : MonoBehaviour
         }
     }
 
+    //Menu() changes the game state to MENU. It then loads the first scene,
+    //which is the title screen/main menu.
     public static void Menu()
     {
         m_State = GAMESTATE.MENU; 
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(_instance.m_TitleSceneName);
 
     }
 }
