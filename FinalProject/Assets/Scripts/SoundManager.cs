f8cc68a6f4568619a7149d64e2bc0c9bee04af67
samgmpurg <117250903+samgmpurg@users.noreply.github.com>
2022-11-26 15:58:35 -0800

    Sound Manager Script (coded)

--- FinalProject/Assets/Scripts/SoundManager.cs
+++ FinalProject/Assets/Scripts/SoundManager.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    [SerializeField]
+    private AudioSource m_SFXAudioSource;
+
+    [SerializeField]
+    private AudioSource m_MusicAudioSource;
+
+    private static SoundManager _instance;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(_instance);
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    public static void PlaySFX(AudioClip clip)
     {
-        
+        _instance.m_SFXAudioSource.PlayOneShot(clip);
+    }
+
+    public static void PlayMusic(AudioClip clip)
+    {
+        _instance.m_MusicAudioSource.Stop();
+        _instance.m_MusicAudioSource.clip = clip;
+        _instance.m_MusicAudioSource.Play();
     }
 }
