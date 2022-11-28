2787e7ec047f7ec80b4500d8997ac77c5fb4cc88
CelestialMagic <116318917+CelestialMagic@users.noreply.github.com>
2022-11-28 15:08:06 -0800

    Updated SwimSpawn List

--- FinalProject/Assets/Scripts/SwimmingObstacleSpawner.cs
+++ FinalProject/Assets/Scripts/SwimmingObstacleSpawner.cs
@@ -8,7 +8,7 @@ public class SwimmingObstacleSpawner : ObstacleSpawner
     private List<float> spawnLocations;
 
 
-    protected virtual void Update()
+    protected override void Update()
     {
         if (nextSpawnTime < Time.time && GameStateManager.CanSpawn == true) //if the nextSpawnTime is NOW or in the past, spawn an obstacle
         {
@@ -16,7 +16,7 @@ public class SwimmingObstacleSpawner : ObstacleSpawner
                 int randomLocation = Random.Range(0, spawnLocations.Count); //Choose a random spawn location from the list
                 //Use Instantiate to create an instance of that template in the game world.
                 GameObject go = Instantiate(obstaclePrefabs[randomObstacle]);
-                go.transform.Translate(randomLocation, spawnYlocation, 0f);
+                go.transform.Translate(spawnLocations[randomLocation], spawnYlocation, 0f);
                 //Select the next time to spawn an obstactle
                 nextSpawnTime += Random.Range(spawnMinTime, spawnMaxTime); //update the nextSpawnTime so we can spawn more!
         }
